c76c0eac0ec89e504799eff0ada6b64bfeeea2cd
David Islam <david.islam08@gmail.com>
2020-06-21 22:13:46 -0700

    Add date picker

--- phase1/src/components/Booking.js
+++ phase1/src/components/Booking.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import DatePicker from './DatePicker';
 
 class Booking extends Component {
   state = {}
   render() {
-    return (<h1>Booking</h1>);
+    return (
+      <div>
+        <h1>Schedule an appointment</h1>
+        <DatePicker label="Choose a date" />
+      </div>
+
+    );
   }
 }
 
