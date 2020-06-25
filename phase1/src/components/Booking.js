1e0c7964ec202c855a92c6808fdf9db5a92c3b70
David Islam <david.islam08@gmail.com>
2020-06-24 17:20:40 -0700

    Pass date to other component

--- phase1/src/components/Booking.js
+++ phase1/src/components/Booking.js
@@ -3,18 +3,33 @@ import Button from '@material-ui/core/Button';
 import DatePicker from './DatePicker';
 import { Link } from 'react-router-dom';
 
+import './booking.css'
+
 class Booking extends Component {
-  state = {}
+  state = { date: new Date() }
+
+  onDateChange = date => {
+    // console.log(date);
+    this.setState({ date })
+  }
+
   render() {
     return (
       <div>
         <h1>Schedule an appointment</h1>
-        <DatePicker label="Choose a date" />
-        <Link to='/centres'>
-          <Button variant="contained" color="primary">
-            Show Assessment Centres
-        </Button>
-        </Link>
+        <DatePicker label="Choose a date" onChange={this.onDateChange} />
+        <div className="container">
+          <Link to={{
+            pathname: '/centres',
+            state: {
+              selected_date: this.state.date.toLocaleDateString()
+            }
+          }} className="link">
+            <Button variant="contained" color="primary">
+              Show Assessment Centres
+            </Button>
+          </Link>
+        </div>
       </div>
     );
   }
