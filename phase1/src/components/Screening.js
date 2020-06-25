0c3f75f06bab0316a939b747593a2d40d398fda5
Jack Ran <sheffield@Jacks-MacBook-Pro.local>
2020-06-25 13:25:51 -0400

    update screening

--- phase1/src/components/Screening.js
+++ phase1/src/components/Screening.js
@@ -7,7 +7,7 @@ class Screening extends Component {
       <div>
         <h1>COVID-19 Screening Tool</h1>
         <p>This tool can help you understand what to do next about COVID-19. Let’s all look out for each other by knowing our status, trying not to infect others, and reserving care for those in need.</p>
-        <input type="button" onclick="question()" value="Start Screening" />
+        <input type="button" onclick="" value="Start Screening" />
       </div>
     );
   }
