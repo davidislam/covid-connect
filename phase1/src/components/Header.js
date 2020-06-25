37aa34040c286dc3aebd9aaa020ec074c70b2590
GavinDser <gavinwang1998@hotmail.com>
2020-06-25 22:43:56 +0800

    updated Header

--- phase1/src/components/Header.js
+++ phase1/src/components/Header.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import {Link} from "react-router-dom";
+
 
 class Header extends Component {
   state = {}
   render() {
-    return (<h1>Header</h1>);
+    return (
+      <nav>
+        <ul className="navList">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/Centre">
+            <li>Center</li>
+          </Link>
+          <Link to="/FAQs">
+            <li>FAQs</li>
+          </Link>
+          <Link to="/Signup">
+            <li>Sign Up</li>
+          </Link>
+        </ul>
+      </nav>
+
+    );
   }
 }
 
