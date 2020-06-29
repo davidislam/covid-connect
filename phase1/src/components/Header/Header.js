01dca70d07e79dd4c7232965d9cd94d945a65b8d
GavinDser <gavinwang1998@hotmail.com>
2020-06-29 11:52:12 +0800

    modified the space in the header

--- phase1/src/components/Header/Header.js
+++ phase1/src/components/Header/Header.js
@@ -16,33 +16,33 @@ export default function Header() {
 
   return (
     <div style={{background:"#3b6978"}}>
-      <Grid container spacing={2} direction="row" justify="flex-start" alignItems="center">
+      <Grid container spacing={2} direction="row" justify="space-around" alignItems="center">
 
-        <Grid item xs={2}>
+        <Grid item>
           <Link to="/" style={{textDecoration: 'none'}}>
             <Typography variant="h6" className={classes.words}>Home</Typography>
             </Link>
         </Grid>
 
-        <Grid item xs={2}>
+        <Grid item>
           <Link to="/booking" style={{textDecoration: 'none'}}>
             <Typography variant="h6" className={classes.words}>Booking Center</Typography>
             </Link>
         </Grid>
 
-        <Grid item xs={2}>
+        <Grid item>
           <Link to="/Screening" style={{textDecoration: 'none'}}>
             <Typography variant="h6" className={classes.words}>Screening</Typography>
             </Link>
         </Grid>
 
-        <Grid item xs={2}>
+        <Grid item>
           <Link to="/FAQs" style={{textDecoration: 'none'}}>
             <Typography variant="h6" className={classes.words}>FAQs</Typography>
             </Link>
         </Grid>
 
-        <Grid item xs={2}>
+        <Grid item>
           <Link to="/Signin" style={{textDecoration: 'none'}}>
             <Typography variant="h6" className={classes.words}>Sign In</Typography>
             </Link>
