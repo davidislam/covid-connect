dc0e51fa625788fd974a30410a25170014a5b4f1
David Islam <david.islam08@gmail.com>
2020-07-03 18:39:57 -0700

    Add more data

--- phase1/src/data.js
+++ phase1/src/data.js
@@ -85,9 +85,145 @@ export const CENTRES = [
     days: everyday,
     hours: [
       { time: "9:00 - 10:00 AM", is_taken: false },
-      { time: "11:00 - 12:00 AM", is_taken: false },
+      { time: "11:00 - 12:00 PM", is_taken: false },
       { time: "1:00 - 2:00 PM", is_taken: false },
       { time: "2:00 - 3:00 PM", is_taken: false }
+    ]
+  },
+  {
+    name: "William Osler Health System",
+    location: {
+      city: "Brampton",
+      address: "500 Ray Lawson Boulevard",
+      postal_code: "L6W 2Z8"
+    },
+    number: "",
+    website: "https://www.williamoslerhs.ca/patients-and-families/preparing-for-your-visit-or-stay/coronavirus-information-for-patients-families/assessment-centre-for-covid-19",
+    days: everyday,
+    hours: [
+      { time: "7:00 - 8:00 AM", is_taken: false },
+      { time: "11:00 - 12:00 PM", is_taken: false },
+      { time: "1:00 - 2:00 PM", is_taken: false },
+      { time: "5:00 - 6:00 PM", is_taken: false }
+    ]
+  },
+  {
+    name: "Dave Andreychuk Mountain Arena Assessment Centre",
+    location: {
+      city: "Hamilton",
+      address: "25 Hester Street",
+      postal_code: "L9A 2N3"
+    },
+    number: "905-974-9848",
+    website: "https://www.hamilton.ca/coronavirus/assessment-centres",
+    days: everyday,
+    hours: [
+      { time: "10:00 - 11:00 AM", is_taken: false },
+      { time: "11:00 - 12:00 PM", is_taken: false },
+      { time: "1:00 - 2:00 PM", is_taken: false },
+      { time: "6:00 - 7:00 PM", is_taken: false }
+    ]
+  },
+  {
+    name: "Hamilton Health Sciences",
+    location: {
+      city: "Hamilton",
+      address: "690 Main Street West",
+      postal_code: "L8S 1A4"
+    },
+    number: "905-974-9848",
+    website: "https://www.hamilton.ca/coronavirus/assessment-centres",
+    days: workdays,
+    hours: [
+      { time: "7:00 - 8:00 AM", is_taken: false },
+      { time: "8:00 - 9:00 AM", is_taken: false },
+      { time: "1:00 - 2:00 PM", is_taken: false },
+      { time: "2:00 - 3:00 PM", is_taken: false }
+    ]
+  },
+  {
+    name: "Brewer Park Arena",
+    location: {
+      city: "Ottawa",
+      address: "151 Brewer Way",
+      postal_code: "K1S 5T1"
+    },
+    number: "",
+    website: "https://www.ottawapublichealth.ca/en/public-health-topics/novel-coronavirus.aspx",
+    days: everyday,
+    hours: [
+      { time: "7:00 - 8:00 AM", is_taken: false },
+      { time: "8:00 - 9:00 AM", is_taken: false },
+      { time: "1:00 - 2:00 PM", is_taken: false },
+      { time: "2:00 - 3:00 PM", is_taken: false }
+    ]
+  },
+  {
+    name: "Queensway Carlton Hospital",
+    location: {
+      city: "Ottawa",
+      address: "595 Moodie Drive",
+      postal_code: "K2H 8A8"
+    },
+    number: "",
+    website: "https://www.ottawapublichealth.ca/en/public-health-topics/novel-coronavirus.aspx",
+    days: workdays,
+    hours: [
+      { time: "9:00 - 10:00 AM", is_taken: false },
+      { time: "10:00 - 11:00 AM", is_taken: false },
+      { time: "3:00 - 4:00 PM", is_taken: false },
+      { time: "4:00 - 5:00 PM", is_taken: false }
+    ]
+  },
+  {
+    name: "NEOMO Medical",
+    location: {
+      city: "Sudbury",
+      address: "885 Prete Street",
+      postal_code: "P3E 3X9"
+    },
+    number: "705-671-7373",
+    website: "https://www.hsnsudbury.ca/portalen/Patients-and-Visitors/COVID-19/COVID-19-Assessment-Centre",
+    days: everyday,
+    hours: [
+      { time: "9:00 - 10:00 AM", is_taken: false },
+      { time: "10:00 - 11:00 AM", is_taken: false },
+      { time: "6:00 - 7:00 PM", is_taken: false },
+      { time: "8:00 - 9:00 PM", is_taken: false }
+    ]
+  },
+  {
+    name: "Primacy Medical Centre",
+    location: {
+      city: "Sudbury",
+      address: "1485 Lasalle Boulevard",
+      postal_code: "P3A 1Z8"
+    },
+    number: "705-671-7373",
+    website: "https://www.hsnsudbury.ca/portalen/Patients-and-Visitors/COVID-19/COVID-19-Assessment-Centre",
+    days: everyday,
+    hours: [
+      { time: "6:00 - 7:00 AM", is_taken: false },
+      { time: "7:00 - 8:00 AM", is_taken: false },
+      { time: "1:00 - 2:00 PM", is_taken: false },
+      { time: "4:00 - 5:00 PM", is_taken: false }
+    ]
+  },
+  {
+    name: "St. Mary's General Hospital - Bathurst Site",
+    location: {
+      city: "Waterloo",
+      address: "50 Bathurst Drive, Unit 1",
+      postal_code: "N2V 2C5"
+    },
+    number: "519-885-9517",
+    website: "https://www.smgh.ca/covid-19-testing-available-for-k-w-residents-with-symptoms-starting-may-16",
+    days: everyday,
+    hours: [
+      { time: "6:00 - 7:00 AM", is_taken: false },
+      { time: "7:00 - 8:00 AM", is_taken: false },
+      { time: "8:00 - 9:00 AM", is_taken: false },
+      { time: "9:00 - 10:00 AM", is_taken: false }
     ]
   }
 ]
