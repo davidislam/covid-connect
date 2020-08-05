b86c0995eef3b5e0b3222fcece1e664d3e6261d8
mingy <milliene.xu@mail.utoronto.ca>
2020-08-05 15:44:19 -0400

    server code some 2?

--- models/user.js
+++ models/user.js
@@ -1,1 +1,68 @@
+'use strict';
+
 const mongoose = require('mongoose');
+const validator = require('validator')
+const bcrypt = require('bcryptjs') // "slow hash", more secure
+
+const UserSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true,
+		unique: true,
+		validate: {
+			validator: validator.isEmail,
+			message: 'Not valid email'
+		}
+	}, 
+	password: {
+		type: String,
+		required: true,
+		minlength: 6
+	}
+})
+
+// Hashing, security, exports before saving to database
+UserSchema.pre('save', function(next) {
+	const user = this; // binds this to User document instance
+
+	// checks to ensure we don't hash password more than once
+	if (user.isModified('password')) {
+		// generate salt and hash the password
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(user.password, salt, (err, hash) => {
+				user.password = hash
+				next()
+			})
+		})
+	} else {
+		next()
+	}
+})
+
+// METHODS -----------------------------------------------------------
+UserSchema.statics.findByEmailPassword = function(email, password) {
+	const User = this
+
+	// Find the user by email
+	return User.findOne({ email: email }).then((user) => {
+		if (!user) {
+			return Promise.reject()  // a rejected promise
+		}
+		// if the user exists, make sure their password is correct
+		return new Promise((resolve, reject) => {
+			bcrypt.compare(password, user.password, (err, result) => {
+				if (result) {
+					resolve(user)
+				} else {
+					reject()
+				}
+			})
+		})
+	})
+}
+
+// Exporting for use
+const User = mongoose.model('User', UserSchema)
+module.exports = { User }
--- server.js
+++ server.js
@@ -7,20 +7,81 @@ const app = express();
 
 const { mongoose } = require('./db/mongoose');
 mongoose.set('bufferCommands', false);  // don't buffer db requests if the db server isn't connected - minimizes http requests hanging if this is the case.
+mongoose.set('useFindAndModify', false); // for some deprecation issues
+
+const { User } = require("./models/user");
 
 const { ObjectID } = require('mongodb');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+// express-session for managing user sessions
+const session = require("express-session");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 function isMongoError(error) { // checks for first error returned by promise rejection if Mongo database suddently disconnects
   return typeof error === 'object' && error !== null && error.name === "MongoNetworkError"
 }
 
+/*** Session Handling ************************************/
+
+// Session cookie
+app.use(
+  session({
+      secret: "oursecret",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+          expires: 60000,
+          httpOnly: true
+      }
+  })
+);
+
+// Login
+app.post("/users/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  log(email, password);
+
+  User.findByEmailPassword(email, password)
+      .then(user => {
+          // Add user ID
+          req.session.user = user._id;
+          req.session.email = user.email;
+          res.send({ currentUser: user.email });
+      })
+      .catch(error => {
+          res.status(400).send()
+      });
+});
+
+// Logout
+app.get("/users/logout", (req, res) => {
+  // Remove the session
+  req.session.destroy(error => {
+      if (error) {
+          res.status(500).send(error);
+      } else {
+          res.send()
+      }
+  });
+});
+
+// Check status
+app.get("/users/check-session", (req, res) => {
+  if (req.session.user) {
+      res.send({ currentUser: req.session.email });
+  } else {
+      res.status(401).send();
+  }
+});
+
 /*********************************************************/
 
 /*** API Routes below ************************************/
-
 
 
 
