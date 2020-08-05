2c9fff0dd42afa4ba49b9c48cbfe4cc06682a2c5
David Islam <david.islam08@gmail.com>
2020-08-05 16:49:07 -0700

    Create centre schema

--- models/centre.js
+++ models/centre.js
@@ -1,1 +1,76 @@
+'use strict';
+
 const mongoose = require('mongoose');
+const validator = require('validator');
+
+const TimeslotSchema = new mongoose.Schema({
+  time: String,
+  isTaken: Boolean
+})
+
+// Assessment centre schema
+const CentreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    trim: true,
+    unique: true
+  },
+  location: {
+    address: {
+      type: String,
+      required: true,
+      minlength: 5,
+      trim: true
+    },
+    city: {
+      type: String,
+      required: true,
+      minlength: 1,
+      trim: true
+    },
+    postalCode: {
+      type: String,
+      required: true,
+      minlength: 6,
+      validate: {
+        validator: validator.isPostalCode(s, "CA"),
+        message: 'Invalid postal code'
+      }
+    },
+    lattitude: {
+      type: Number,
+      required: true,
+    },
+    longitude: {
+      type: Number,
+      required: true
+    }
+  },
+  phoneNumber: {
+    type: String,
+    minlength: 10,
+  },
+  url: {
+    type: String,
+    minlength: 1,
+    validate: {
+      validator: validator.isURL,
+      message: 'Invalid url'
+    }
+  },
+  hours: {
+    monday: [TimeslotSchema],
+    tuesday: [TimeslotSchema],
+    wednesday: [TimeslotSchema],
+    thursday: [TimeslotSchema],
+    friday: [TimeslotSchema],
+    saturday: [TimeslotSchema],
+    sunday: [TimeslotSchema],
+  }
+})
+
+const Centre = mongoose.model('Centre', CentreSchema);
+module.exports = { Centre };
+
