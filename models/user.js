c3b305eb77616b49db52a4629e29f2c83c690f43
David Islam <david.islam08@gmail.com>
2020-08-10 07:32:20 -0700

    Change user model

--- models/user.js
+++ models/user.js
@@ -5,24 +5,24 @@ const validator = require('validator')
 const bcrypt = require('bcryptjs') // "slow hash", more secure
 
 const UserSchema = new mongoose.Schema({
-    username: {
-		type: String,
-		required: true,
-        minlength: 6,
-        trim: true,
-        unique: true,
-    },
-	email: {
+	username: {
 		type: String,
 		required: true,
 		minlength: 1,
 		trim: true,
-		unique: true,
-		validate: {
-			validator: validator.isEmail,
-			message: 'Not valid email'
-		}
-	}, 
+		unique: true
+	},
+	// email: {
+	// 	type: String,
+	// 	required: true,
+	// 	minlength: 1,
+	// 	trim: true,
+	// 	unique: true,
+	// 	validate: {
+	// 		validator: validator.isEmail,
+	// 		message: 'Not valid email'
+	// 	}
+	// }, 
 	password: {
 		type: String,
 		required: true,
@@ -31,7 +31,7 @@ const UserSchema = new mongoose.Schema({
 })
 
 // Hashing, security, exports before saving to database
-UserSchema.pre('save', function(next) {
+UserSchema.pre('save', function (next) {
 	const user = this; // binds this to User document instance
 
 	// checks to ensure we don't hash password more than once
@@ -49,32 +49,32 @@ UserSchema.pre('save', function(next) {
 })
 
 // METHODS -----------------------------------------------------------
-UserSchema.statics.findByEmailPassword = function(email, password) {
-	const User = this
+// UserSchema.statics.findByEmailPassword = function(email, password) {
+// 	const User = this
 
-	// Find the user by email
-	return User.findOne({ email: email }).then((user) => {
-		if (!user) {
-			return Promise.reject()  // a rejected promise
-		}
-		// if the user exists, make sure their password is correct
-		return new Promise((resolve, reject) => {
-			bcrypt.compare(password, user.password, (err, result) => {
-				if (result) {
-					resolve(user)
-				} else {
-					reject()
-				}
-			})
-		})
-	})
-}
+// 	// Find the user by email
+// 	return User.findOne({ email: email }).then((user) => {
+// 		if (!user) {
+// 			return Promise.reject()  // a rejected promise
+// 		}
+// 		// if the user exists, make sure their password is correct
+// 		return new Promise((resolve, reject) => {
+// 			bcrypt.compare(password, user.password, (err, result) => {
+// 				if (result) {
+// 					resolve(user)
+// 				} else {
+// 					reject()
+// 				}
+// 			})
+// 		})
+// 	})
+// }
 
-UserSchema.statics.findByUsernamePassword = function(username, password) {
+UserSchema.statics.findByUsernamePassword = function (username, password) {
 	const User = this
 
 	// Find the user by username
-	return User.findOne({ username: username }).then((user) => {
+	return User.findOne({ username }).then((user) => {
 		if (!user) {
 			return Promise.reject()  // a rejected promise
 		}
