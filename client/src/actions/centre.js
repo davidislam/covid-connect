84488944b1db1fecc9151fab613d030ece2ae648
David Islam <david.islam08@gmail.com>
2020-08-12 14:31:11 -0700

    Add getCentresByCityForDay server call

--- client/src/actions/centre.js
+++ client/src/actions/centre.js
@@ -1,11 +1,13 @@
 import { CENTRES } from '../data';
 import axios from 'axios';
-import { handleError } from './../utils';
+import { handleError, days, months } from './../utils';
 
 
 const api = axios.create({
   baseURL: 'http://localhost:5000'
 })
+
+const log = console.log;
 
 export function modifyCentre(centre, hours, lat, lng) {
   const centreToModify = CENTRES.filter(c => c._id === centre.centreID)[0];
@@ -66,6 +68,29 @@ export function getCentres(comp) {
     })
 }
 
+export function getCentresByCityForDay(comp, city, date) {
+  const day = getDay(date);
+  const dateStr = date.toLocaleDateString();
+
+  api.get(`/centres/city/${city}`, {
+    transformResponse: data => {
+      const filteredCentres = JSON.parse(data).filter(centre => centre.hours[day].length !== 0)
+      // log(filteredCentres);
+      return filteredCentres;
+    }
+  })
+    .then(res => {
+      const centres = res.data;
+      const message = centres.length === 0 ? 'No centres found' :
+        `COVID-19 assessment centres in ${city} for ${dateStr}`;
+      comp.setState({ centres, message })
+    })
+    .catch(error => {
+      alert(`Could not get centres in ${city} for ${dateStr}`);
+      handleError(error);
+    })
+}
+
 // Returns centre with name <name>
 export function getCentreByName(name) {
   return CENTRES.filter(centre => centre.name === name)[0];
@@ -74,3 +99,28 @@ export function getCentreByName(name) {
 export function addTimeslot(centre, day, time) {
   // /centres/:id/:day/:time
 }
+
+// Returns a formatted date
+export function formattedDate(date) {
+  const monthNum = date.getMonth();
+  const monthName = months[monthNum];
+  return `${monthName} ${date.getDate()}, ${date.getFullYear()}`
+}
+
+// Returns a day of the week
+export function getDay(date) {
+  return days[date.getDay()].toLowerCase();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- client/src/components/Centre/index.js
+++ client/src/components/Centre/index.js
@@ -16,35 +16,11 @@ class AssessmentCentres extends Component {
     getCentresByCityForDay(this, city, date);
   }
 
-  // filterCentres() {
-  //   if (!this.props.location) {
-  //     console.log("location is undefined");
-  //     return;
-  //   }
-  //   let { selected_date, selected_city } = this.props.location.state;
-  //   const day = getDay(selected_date);
-  //   const filteredCentres = CENTRES.filter(c => {
-  //     return c.hours[day].length !== 0 && c.location.city === selected_city;
-  //   })
-  //   this.setState({
-  //     centres: filteredCentres
-  //   })
-  // }
-
   render() {
     const { selected_date } = this.props.location.state;
     return (
       <div className='centres'>
-        {/* <h3>COVID-19 assessment centres in {selected_city} for {selected_date.toLocaleDateString()}</h3> */}
         <h3>{this.state.message}</h3>
-        {/* {this.state.centres.length !== 0 ?
-          <CentreList
-            centres={this.state.centres}
-            addAppt={(appt) => this.props.addAppt(appt)}
-            formattedDate={formattedDate(selected_date)}
-            day={this.day}
-            isLoggedIn={this.props.isLoggedIn} /> :
-          <h3>No centres found</h3>} */}
         <CentreList
           centres={this.state.centres}
           addAppt={(appt) => this.props.addAppt(appt)}
