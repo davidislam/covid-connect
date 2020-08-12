6843470d690688e8752fd7bec2e0abeedebb75b6
David Islam <david.islam08@gmail.com>
2020-08-11 22:03:19 -0700

    GET city names

--- client/src/components/Booking/BookingInfo.js
+++ client/src/components/Booking/BookingInfo.js
@@ -3,19 +3,29 @@ import Button from '@material-ui/core/Button';
 import DatePicker from './DatePicker';
 import CitySelect from './CitySelect';
 import { Link } from 'react-router-dom';
-import { CITIES } from './../../data';
+// import { CITIES } from './../../data';
 import Map from './Map';
+import axios from 'axios';
 
 import './styles.css'
+
+const api = axios.create({
+  baseURL: 'http://localhost:5000'
+})
+
 
 class Booking extends Component {
   state = {
     date: new Date(),
-    city: ''
+    city: '',
+    cities: []
   }
 
   componentDidMount() {
-    // Fetch cities data from an API
+    api.get('/city')
+      .then(res => {
+        this.setState({ cities: res.data });
+      })
   }
 
   handleChange = e => {
@@ -25,17 +35,17 @@ class Booking extends Component {
   }
 
   render() {
-    const { city, date } = this.state;
+    const { city, date, cities } = this.state;
     return (
       <div>
         {this.props.isLoggedIn ? <h1>Hi {this.props.username}!</h1> : <h1>Hi!</h1>}
         <p>Find your closest Ontario assessment centre to get a COVID-19 (coronavirus) test.</p>
         <p>All assessment centres will test you if you would like to be tested. Testing is a free service.</p>
         <p>Call the assessment centre or your <a href="http://www.health.gov.on.ca/en/common/system/services/phu/locations.aspx">local public health unit</a> if you have questions or cannot find a centre near you.</p>
-        <Map />
+        {/* <Map /> */}
         <h2>Schedule an appointment</h2>
         <DatePicker label="Choose date" value={date} onChange={(date) => this.setState({ date })} />
-        <CitySelect label="City" value={city} cities={CITIES} onChange={this.handleChange} />
+        <CitySelect label="City" value={city} cities={cities} onChange={this.handleChange} />
         <div className="container">
           <Link to={{
             pathname: '/centres',
