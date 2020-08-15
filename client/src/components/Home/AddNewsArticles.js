04045c8a9fe751dc5c44c217ba4b6eb00b797fad
mingy <milliene.xu@mail.utoronto.ca>
2020-08-15 13:10:27 -0400

    in progrsss AKJDLKAJA

---
+++ client/src/components/Home/AddNewsArticles.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import CustomizedSnackbar from '../../CustomizedSnackbar';
+
+import { addNewsArticle } from '../../../actions/news'
+//import { createCentre, timeslotAdded } from '../../../actions/centre';
+import { toggle, handleChange } from '../../../utils';
+import DialogWindowAddCentre from './DialogWindowAddCentre';
+
+export default class AddCentre extends Component {
+  state = {
+    link: "",
+    image: "",
+    heading: "",
+
+    snackbarMessage: "",
+    snackbarSeverity: "",
+    snackbarOpen: false,
+  };
+
+  // componentDidMount() {
+  //   this.hours = {
+  //     monday: [], tuesday: [], wednesday: [], thursday: [], friday: [],
+  //     saturday: [], sunday: []
+  //   };
+  // }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      const { link, image, heading } = this.state;
+      const newArticle = {link, image, heading}
+      addNewsArticle(this, newArticle);
+    } catch (error) {
+      console.error(error);
+      this.setState({ snackbarOpen: true, snackbarMessage: "Incomplete address", snackbarSeverity: "warning" });
+    }
+  };
+
+  render() {
+    const { snackbarMessage, snackbarOpen, snackbarSeverity } = this.state;
+    return (
+      <React.Fragment>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => toggle(this, "open")}
+        >
+          Add Assessment Centre
+        </Button>
+        <DialogWindowAddCentre
+          onClose={() => toggle(this, "open")}
+          onChange={(e) => handleChange(this, e)}
+          handleSubmit={this.handleSubmit}
+          title='Add Centre'
+          heading='To add an assessment centre to this website, please fill out the form below.'
+          comp={this}
+        />
+        <CustomizedSnackbar
+          message={snackbarMessage}
+          severity={snackbarSeverity}
+          open={snackbarOpen}
+          toggleSnackbar={() => toggle(this, "snackbarOpen")}
+        />
+      </React.Fragment>
+    );
+  }
+}
